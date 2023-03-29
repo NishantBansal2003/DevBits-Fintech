be4929ec20f92d206ccaf2db9abb08879133b842
Nishant Bansal <nishant.bansal.mec21@iitbhu.ac.in>
2023-03-29 12:15:09 +0000

    Commiting ContactUs page

---
+++ ContactUs/render.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header.jsx";
+import ConatctUs from "./components/ContactUs.jsx";
+import Footer from "./components/footer";
+import "./styles/index.css";
+function Render() {
+  return (
+    <div>
+      <Header />
+      <ConatctUs />
+      <Footer />
+    </div>
+  );
+}
+export default Render;
